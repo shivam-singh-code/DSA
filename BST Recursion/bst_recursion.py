065783fb49ba23d9686c623cdacef82902a9c198
shivam-singh-code <shivamsingh97166@gmail.com>
2025-07-08 22:59:33 +0530

    bst recursion & sorting

--- BST Recursion/bst_recursion.py
+++ BST Recursion/bst_recursion.py
@@ -73,9 +73,39 @@ class BinarySearchTree():
         if value > current_node.value:
             current_node.right = self.__r_insert(current_node.right, value)
         return current_node
+   
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return
+       
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+   
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
 
 
-my_bst = BinarySearchTree()
+    def delete_node(self, value):
+        self.__delete_node(self.root, value)
+
+
+# my_bst = BinarySearchTree()
 # my_bst.insert(47)
 # my_bst.insert(21)
 # my_bst.insert(76)
@@ -93,13 +123,72 @@ my_bst = BinarySearchTree()
 # print(my_bst.contain(17))
 
 
-my_bst.r_insert(2)
-my_bst.r_insert(1)
-my_bst.r_insert(3)
+# my_bst.r_insert(2)
+# my_bst.r_insert(1)
+# my_bst.r_insert(3)
 
 
-print("--Printing result value now")
-print('Root: value', my_bst.root.value)
-print('Root left: ', my_bst.root.left.value)
-print('Root right: ', my_bst.root.right.value)
+# print("--Printing result value now")
+# print('Root: value', my_bst.root.value)
+# print('Root left: ', my_bst.root.left)
+# print('Root right: ', my_bst.root.right)
 
+
+# print(my_bst.min_value(my_bst.root))
+# print(my_bst.min_value(my_bst.root.right))
+       
+my_tree = BinarySearchTree()
+my_tree.r_insert(2)
+my_tree.r_insert(1)
+my_tree.r_insert(3)
+
+
+"""
+       2
+      / \
+     1   3
+"""
+
+
+print("root:", my_tree.root.value)
+print("root.left =", my_tree.root.left.value)
+print("root.right =", my_tree.root.right.value)
+
+
+
+
+my_tree.delete_node(2)
+
+
+"""
+       3
+      / \
+     1   None
+"""
+
+
+
+
+print("\nroot:", my_tree.root.value)
+print("root.left =", my_tree.root.left.value)
+print("root.right =", my_tree.root.right)
+
+
+
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    root: 2
+    root.left = 1
+    root.right = 3
+
+
+    root: 3
+    root.left = 1
+    root.right = None
+
+
+"""
